e0d64eb90304545a5bd571bcafaa68afc72d6269
Pavlo Tokariev <pavlo.tokariev@inria.fr>
2026-02-27 01:55:47 +0100

    Fix in M2sim, regeneration of traces.

--- refinement/visualize.sh
+++ refinement/visualize.sh
@@ -1,4 +1,4 @@
-declare -a specs=("M0" "M1" "M2sim" "M21" "M22" );
+declare -a specs=("M0" "M1" "M2sim" "M21" "M22");
 declare -a bounds=("0.01" "0.1" "1");
 
 for spec in "${specs[@]}"
@@ -6,8 +6,8 @@ do
     for b in "${bounds[@]}"
     do
         echo "./analysis/$spec/$b/chain/weighted/start1_finish3.histogram.csv";
-        if [ ! -d "./images/$spec/$b/chain/weighted/" ]; then
-            mkdir -p "./images/$spec/$b/chain/weighted/";
+        if [ ! -d "./images/$spec/$b/" ]; then
+            mkdir -p "./images/$spec/$b/";
         fi
         cat "./analysis/$spec/$b/chain/weighted/start1_finish3.histogram.csv" | gnuplot ./visualize.gnu > "./images/$spec/$b/start1_finish3.pdf";
     done
